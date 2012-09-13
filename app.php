cda8e4adfa201db4b84f2f57474adf500ff6784f
Jeremy Manoto <jeremy@manoto.com.au>
2012-09-13 16:15:59 +1000

    Fleshed out API samples for Devices Added Twitter Bootstrap styling

--- app.php
+++ app.php
@@ -2,39 +2,150 @@
 <html lang="en-US">
 <head>
 	<meta charset="UTF-8">
-	<title></title>
+	<title>Example Usage | Ninja Blocks PHP Helper Library</title>
+	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.1.1/css/bootstrap.min.css" rel="stylesheet">
+	<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.1.1/js/bootstrap.min.js"></script>
+
+	<style type="text/css">
+		.code { color: #D14; }
+	</style>
 </head>
 <body>
-	<h1>Ninja Blocks - PHP Helper Library</h1>
+	<div class="container">
+
+		<div class="hero-unit">
+
+			<h1>PHP Helper Library</h1>
+			<p>Example usage of the Ninja Blocks API with PHP</p>
+		</div>
+
+		<p><span class="label label-info">Important!</span> Make sure your ninja block is connected 
+			and live with the cloud</p>
 
 
-	<?php 
+		<?php 
+
+		/**
+		 * Sample Application using the PHP Ninja Blocks helper class.
+		 */
+		
+		// include the Ninja Blocks API library
+		require_once("nbapi.php");
 
 
-	/**
-	 * Sample Application using the PHP Ninja Blocks helper class.
-	 */
+		// Generate your access token from OAUTH2 or your Dashboard
+		// Settings->API->API Access Token
+		$accessToken = "8b7c78a0-9e65-47d8-a9a3-93462c900cf9";
 
-	require_once("ninjablocks.php");
-	require_once("PhpConsole.php");
-	PhpConsole::start();
+		// Instantiate a new Ninjablock
+		$deviceAPI = new Device($accessToken);
+
+		?>
+		<h2>DeviceAPI</h2>
+		<p>Set your access token and instantiate a new Device API to interact with your Ninja Block devices</p>
+		<h5>Code</h5>
+		<p><code>$accessToken = "YOUR TOKEN STRING"</code></p>
+		<p><code>$deviceAPI = new Device($accessToken);</code></p>
 
 
-	$accessToken = "8b7c78a0-9e65-47d8-a9a3-93462c900cf9";
+<?php
+		
+		$devicesResponse = $deviceAPI->getDevices();
+
+?>
+
+		<h4>Get Devices</h4>
+		<h5>Code</h5>
+		<p><code>$devicesAPI->getDevices();</code></p>
+		<h5>Output</h5>
+		<pre class="pre-scrollable"><code><?= print_r($devicesResponse); ?></code></pre>
+
+<?php
+			// target just the data portion of the devices response payload.
+			$devices = $devicesResponse->data;
+
+?>
+
+		<hr/>
+
+<?php
+
+			// Looping over each device.
+			$deviceCount = 0;
+			foreach($devices as $guid => $device) {
+				$deviceCount ++;
+?>
+				<h3>Device <?= $deviceCount ?>: <?= $device->device_name ?></h3>
+				<span class="label label-success">GUID:</span> <?= $guid ?><br>
+				<span class="label label-warning">Type:</span> <?= $device->device_type ?><br>
+				<span class="label label-info">Name:</span> <?= $device->default_name ?><br>
+
+				<h4>Last Heartbeat</h4>
+				<h5>Code</h5>
+				<p><code>$deviceAPI->lastHeartbeat($guid)</code></p>
+				<h5>Output</h5>
+				<pre><code><?= print_r($deviceAPI->lastHeartbeat($guid)); ?></code></pre>
+<?php
 
 
-	$ninjablocks = new NinjaBlocks($accessToken);
+				switch ($device->device_type) {
 
-	//echo "<h2>Authorize</h2>";
-	//echo $ninjablocks->Authorize();
+					case "light":
+						break;
 
-	echo "<h2>User</h2>";
-	echo $ninjablocks->User();
+					case "rgbled":
 
-	echo "<h2>Devices</h2>";
-	echo $ninjablocks->GetDevices();
+?>
+						<h4>Actuating LED</h4>
+						<h5>Code</h5>
+						<pre class="code"><code>$actuateData = (object) array('DA' => "FF00FF", 'shortName' => 'Purple');
+$actuateResponse = $deviceAPI->actuate($guid, $actuateData);</code></pre>
+						<h5>Output</h5>
+						<pre><code><?php
 
-	?>
+						$actuateData = (object) array('DA' => "FF00FF", 'shortName' => 'Purple');
+
+						$actuateResponse = $deviceAPI->actuate($guid, $actuateData);
+						echo print_r($actuateResponse);
+							
+						?></code></pre>
+
+						<h4>Subscribe</h4>
+						<h5>Code</h5>
+						<p><code>$deviceAPI->subscribe($guid, "http://your.callback.url");</code></p>
+						<h5>Output</h5>
+						<pre><code><?php
+
+						$subscribeResponse = $deviceAPI->subscribe($guid, "http://requestb.in/13ozq1w1");
+						echo print_r($subscribeResponse);
+
+						?></code></pre>
+
+						<h4>Unsubscribe</h4>
+						<h5>Code</h5>
+						<p><code>$deviceAPI->unsubscribe($guid);</code></p>
+						<h5>Output</h5>
+						<pre><code><?php
+
+						$unsubscribeResponse = $deviceAPI->unsubscribe($guid);
+						echo print_r($unsubscribeResponse);
+							
+						?></code></pre>
+						<?php
+
+						break;
+
+					case "orientation":
+						break;
+
+				}
+
+
+				//break; // Only process the first one for demonstration purposes
+			}
+
+		?>
+	</div>
 
 </body>
 </html>
---
+++ nbapi.php
@@ -0,0 +1,201 @@
+<?php
+
+/**
+ * Ninja Blocks API Helper Classes.
+ *
+ * This is a Ninja Blocks Helper class to assist php developers with
+ * interacting with the Ninja Blocks REST service
+ *
+ * @author  Jeremy Manoto <jeremy@ninjablocks.com>
+ *
+ * @since 0.1
+ *
+ * @copyright 2012 Ninja Blocks Pty Ltd
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ *
+ * 
+ */
+class NBAPI {
+	var $version = "v0";
+
+	/**
+	 * Url for the Ninja Blocks endpoint.
+	 * @var string
+	 */
+	var $apiUrl;
+
+	/**
+	 * Authentication access token
+	 * @var string
+	 */
+	var $accessToken;
+
+	var $timeout = 300;
+
+	// Constructor
+	public function __construct($token) {
+
+		// Set the access token for the session
+		$this->accessToken = $token;
+
+		// Set the API url. Embed the version number straight in.
+		$this->apiUrl = "https://api.ninja.is/rest/" . $this->version . "/";
+	}
+
+	/**
+	 * USER
+	 */
+	public function User() {
+
+		return $this->MakeRequest("GET", "user");
+
+	}
+
+	public function GetDevices() {
+		return $this->MakeRequest("GET", "devices");
+
+	}
+
+	/**
+	 * Forms & makes an API Request
+	 */
+	public function MakeRequest($method, $endpoint, $data = false) {
+		// Declare the headers
+		$headers = array();
+		array_push($headers, 'Accepts: application/json');
+		array_push($headers, 'Content-Type: application/json');
+
+		// Initialize Curl
+		$curl = curl_init();
+
+		// Switch out the methods for communicating with the API
+		switch ($method) {
+			case "GET":
+				if ($data) {
+					$urlData = http_build_query($data);
+				}
+				break;
+			case "POST":
+				curl_setopt($curl, CURLOPT_POST, 1);
+
+				if ($data) {
+					curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+					array_push($headers, 'Content-Length: ' . strlen($data));
+				}
+				break;
+			case "PUT":
+				curl_setopt($curl, CURLOPT_PUT, 1);
+				if ($data) {
+					curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+					array_push($headers, 'Content-Length: ' . strlen($data));
+					echo "PUT Data: {$data}";
+				}
+				break;
+			case "DELETE":
+				curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
+				if ($data) {
+					curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+					array_push($headers, 'Content-Length: ' . strlen($data));
+				}
+
+				break;
+		}
+
+		// Generate the final endpoint url to be called
+		$url = "{$this->apiUrl}{$endpoint}?user_access_token={$this->accessToken}";
+		
+		// Set the curl options
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_HEADER, true);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($curl, CURLOPT_VERBOSE, true);
+
+
+		// Make the call
+		$response = curl_exec($curl);
+		//echo print_r(curl_getinfo($curl));
+
+		$headerSize = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+		$header = substr($response, 0, $headerSize);
+		$body = substr($response, $headerSize);
+	
+		$curlInfo = curl_getinfo($curl);
+		//echo print_r($curlInfo);
+
+		// Close the connection
+		curl_close($curl);
+
+		// Convert to JSON object
+		$responseJSON = json_decode($body);
+
+		return $responseJSON;
+	}
+
+
+}
+
+class User {
+
+	public function __construct($accessToken) {
+		$this->nbapi = new NBAPI($accessToken);
+	}
+
+	public function GetUser() {
+
+	}
+
+}
+
+
+
+class Device {
+
+	var $nbapi;
+
+	function __construct($accessToken) {
+
+		$this->nbapi = new NBAPI($accessToken);
+
+	}
+
+	/**
+	 * Lists the devices associated with the authenticating user
+	 * @return object 
+	 */
+	public function getDevices() {
+		return $this->nbapi->MakeRequest("GET", "devices");
+	}
+
+
+	/**
+	 * Actuates a device
+	 * @param  string $guid The GUID of the device to actuate
+	 * @param  object $da   The data object to pass to the device
+	 * @return object       
+	 */
+	public function actuate($guid, $da) {
+		return $this->nbapi->MakeRequest("PUT", "device/{$guid}", json_encode($da));
+	}
+
+	public function subscribe($guid, $url) {
+
+		$urlObject = (object) array('url' => $url);
+		return $this->nbapi->MakeRequest("POST", "device/{$guid}/callback", json_encode($urlObject));
+	}
+
+	public function unsubscribe($guid) {
+		return $this->nbapi->MakeRequest("DELETE", "device/{$guid}/callback");
+	}
+
+	public function data($guid, $from, $to) {
+
+		$dataScope = (object) array('from' => $from, 'to' => $to);
+		return $this->nbapi->MakeRequest("GET", "device/{$guid}/data". $dataScope);
+	}
+
+	public function lastHeartbeat($guid) {
+		return $this->nbapi->MakeRequest("GET", "device/{$guid}/heartbeat");
+	}
+}
